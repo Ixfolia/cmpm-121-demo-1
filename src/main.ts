a7e06ae6e22e86adcda8b8562b2e3347393c5873
Brady Lin <linbrady16877@gmail.com>
2024-10-08 21:27:44 -0700

    Step 2 complete
    Added counter div in HTML. Attached button to the counter by using document.querySelector('#counter-display'). Created a function that increases the counter variable and attached the variable to the HTML counter div.

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const header = document.createElement("h1");
 const gameName = "Cool Game 1";
 const button = document.createElement("button");
+let counter: number = 0;
+const counterDisplay = document.querySelector('#counter-display') as HTMLDivElement;
 
-// - Set HTML Element Properties - //
+// -- Set HTML Element Properties -- //
 header.innerHTML = gameName;
 button.innerHTML = "Click me 🤑";
 
@@ -17,3 +19,10 @@ document.title = gameName;
 app.append(header);
 app.append(button);
 
+function updateCounter(): void {
+    counter++;
+    counterDisplay.textContent = `${counter} dollars 💰`; // Display the updated counter
+}
+
+// Add event listener to the button, updates the counter in HTML
+button.addEventListener('click', updateCounter);
