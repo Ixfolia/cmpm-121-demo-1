3a6518da369e4b54f085cc0b8fa68574d3d39fb6
Brady Lin <linbrady16877@gmail.com>
2024-10-08 18:29:24 -0700

    Step 1 complete
    Added <button> to the webpage via TypeScript. Reorganized code and added some header texts to them.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import "./style.css";
 
+// -- Variables -- //
 const app: HTMLDivElement = document.querySelector("#app")!;
-
+const header = document.createElement("h1");
 const gameName = "Cool Game 1";
+const button = document.createElement("button");
+
+// - Set HTML Element Properties - //
+header.innerHTML = gameName;
+button.innerHTML = "Click me 🤑";
+
+// Title
 document.title = gameName;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
+// -- Functions -- //
 app.append(header);
+app.append(button);
+
